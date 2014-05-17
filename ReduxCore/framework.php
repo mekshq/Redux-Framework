92063dbcf2193e52f74d21e9f026960d367c4d45
Bojan <belgrade.celt@gmail.com>
2014-05-17 02:56:14 +0200

    testing

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -27,6 +27,8 @@ if( !defined( 'ABSPATH' ) ) {
     exit;
 }
 
+//Test comment
+
 // Fix for the GT3 page builder: http://www.gt3themes.com/wordpress-gt3-page-builder-plugin/
 /** @global string $pagenow */
 if(has_action('ecpt_field_options_')) {
